a96261609c626d4d83fa5d4b6e9683dd99be9cb4
Chittamur Diwakar Reddy <diwakarreddy81@gmail.com>
2022-11-21 06:19:28 +0530

    Signed-off-by: Chittamur Diwakar Reddy <diwakarreddy81@gmail.com>

---
+++ JAVA-PROGRAME-PACKAGES/JAVAPROJECTPACKAGES/src/com/NumberProgramspackages/SwapNumber.java
@@ -0,0 +1,15 @@
+package com.NumberProgramspackages;
+
+public class SwapNumber 
+{
+	public static void main(String[] args) 
+	{
+		int a =10;
+		int b = 20;
+		System.out.println("Befor Swap a="+a + "b="+b);
+		int temp=a;
+		 a=b;
+		 b=temp;
+		System.out.println("After Swap a="+a + "b="+b);
+	}
+}
---
+++ JAVA-PROGRAME-PACKAGES/JAVAPROJECTPACKAGES/src/module-info.java
@@ -0,0 +1,9 @@
+/**
+ * 
+ */
+/**
+ * @author diwak
+ *
+ */
+module JAVAPROJECTPACKAGES {
+}
